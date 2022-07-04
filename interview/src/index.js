e08f7880bac2c9f96be14ddd618e45adff92591d
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-04 15:11:52 +0300

    userSlice

--- interview/src/index.js
+++ interview/src/index.js
@@ -3,7 +3,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'normalize.css';
+import { store } from 'react-redux';
+import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
+);
