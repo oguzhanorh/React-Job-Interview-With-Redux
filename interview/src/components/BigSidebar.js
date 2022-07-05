b91546cd30913992272f4036f7d58519e11d48af
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-05 23:37:22 +0300

    BigSidebar create

--- interview/src/components/BigSidebar.js
+++ interview/src/components/BigSidebar.js
@@ -1,9 +1,27 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import { useSelector } from 'react-redux';
 
 export const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
   return (
     <Wrapper>
-      <h1>BigSidebar</h1>
+      <div
+        className={
+          isSidebarOpen
+            ? 'sidebar-container '
+            : 'sidebar-container  show-sidebar'
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
