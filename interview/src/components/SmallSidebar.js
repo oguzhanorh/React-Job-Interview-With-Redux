bd57646e6692a97332c4d35d6fe612ea181ef474
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-05 23:03:12 +0300

    create smallSidebar

--- interview/src/components/SmallSidebar.js
+++ interview/src/components/SmallSidebar.js
@@ -1,9 +1,32 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
-
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
 export const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
   return (
     <Wrapper>
-      <h1>SmallSidebar</h1>
+      <div
+        className={
+          isSidebarOpen ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
