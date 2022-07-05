f5327ac32a24b4c00eddf99f91de8ed91719ad90
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-05 22:13:32 +0300

    icons

--- interview/src/components/Navbar.js
+++ interview/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 
 export const Navbar = () => {
   return (
     <Wrapper>
       <h2>Navbar</h2>
+      <FaHome />
     </Wrapper>
   );
 };
