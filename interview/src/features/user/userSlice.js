d462fa5c8dc56a6591c340f54b45bae9f0fb0dc8
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-12 13:11:47 +0300

    set authorize status 401

--- interview/src/features/user/userSlice.js
+++ interview/src/features/user/userSlice.js
@@ -46,12 +46,17 @@ export const updateUser = createAsyncThunk(
     try {
       const resp = await customFetch.patch('/auth/updateUser', user, {
         headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+          // authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+          authorization: `Bearer`,
         },
       });
       return resp.data;
     } catch (error) {
-      console.log(error.response);
+      //console.log(error.response);
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser);
+        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
