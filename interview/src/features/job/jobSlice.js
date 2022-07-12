ab36bc327f084296feb4b5752c8471096e4a8cb7
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-12 14:45:07 +0300

    clear values in addjob page

--- interview/src/features/job/jobSlice.js
+++ interview/src/features/job/jobSlice.js
@@ -24,9 +24,14 @@ const jobSlice = createSlice({
     handleChange: (state, { payload: { name, value } }) => {
       state[name] = value;
     },
+    clearValues: () => {
+      return {
+        ...initialState,
+      };
+    },
   },
 });
 
-export const { handleChange } = jobSlice.actions;
+export const { handleChange, clearValues } = jobSlice.actions;
 
 export default jobSlice.reducer;
--- interview/src/pages/dashboard/Addjob.js
+++ interview/src/pages/dashboard/Addjob.js
@@ -3,7 +3,7 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange } from '../../features/job/jobSlice';
+import { handleChange, clearValues } from '../../features/job/jobSlice';
 
 export const AddJob = () => {
   const {
@@ -78,7 +78,7 @@ export const AddJob = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log('clear values')}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
