a89c8049febea56c008b9a202e6fa4a864e47a61
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-13 16:56:10 +0300

    create a demo user

--- interview/src/pages/Register.js
+++ interview/src/pages/Register.js
@@ -84,6 +84,18 @@ const Register = () => {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           {isLoading ? 'loading...' : 'submit'}
         </button>
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          disabled={isLoading}
+          onClick={() =>
+            dispatch(
+              loginUser({ email: 'testUser@test.com', password: 'secret' })
+            )
+          }
+        >
+          {isLoading ? 'loading...' : 'demo app'}
+        </button>
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
 
