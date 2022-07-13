79cee5bc447dbebc88d56f69fb0c9ed987598bcb
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-13 17:38:14 +0300

    create stats page

--- interview/src/features/alljobs/allJobsSlice.js
+++ interview/src/features/alljobs/allJobsSlice.js
@@ -39,6 +39,19 @@ export const getAllJobs = createAsyncThunk(
   }
 );
 
+export const showStats = createAsyncThunk(
+  'allJobs/showStats',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await customFetch.get('/jobs/stats');
+      console.log(resp.data);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
 const allJobsSlice = createSlice({
   name: 'allJobs',
   initialState,
@@ -60,6 +73,18 @@ const allJobsSlice = createSlice({
       state.jobs = payload.jobs;
     },
     [getAllJobs.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      toast.error(payload);
+    },
+    [showStats.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [showStats.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.stats = payload.defaultStats;
+      state.monthlyApplications = payload.monthlyApplications;
+    },
+    [showStats.rejected]: (state, { payload }) => {
       state.isLoading = false;
       toast.error(payload);
     },
--- interview/src/pages/dashboard/Stats.js
+++ interview/src/pages/dashboard/Stats.js
@@ -1,5 +1,10 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { showStats } from '../../features/alljobs/allJobsSlice';
 export const Stats = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(showStats());
+  }, []);
   return <div>Stats</div>;
 };
