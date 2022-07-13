3f3fc254b9ad5c5d07eea30cae66db9d087ce927
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-13 11:53:42 +0300

    alljobs container setup with loading

--- interview/src/components/JobsContainer.js
+++ interview/src/components/JobsContainer.js
@@ -1,5 +1,34 @@
 import React from 'react';
-
+import { useEffect } from 'react';
+import { Job } from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from './Loading';
 export const JobsContainer = () => {
-  return <div>JobsContainer</div>;
+  const { jobs, isLoading } = useSelector((store) => store.alljobs);
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+  //kaç tane job bulursa onun bilgilerini aşağıda ki komut göstericek.
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job.id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
+export default JobsContainer;
