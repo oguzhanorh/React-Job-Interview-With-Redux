9d137206f1aaad878d647d69d7b6825c56441046
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-14 12:22:33 +0300

    change page btn

--- interview/src/components/PageBtnContainer.js
+++ interview/src/components/PageBtnContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 import Wrapper from '../assets/wrappers/PageBtnContainer';
 import { useSelector, useDispatch } from 'react-redux';
+import { changePage } from '../features/alljobs/allJobsSlice';
 
 export const PageBtnContainer = () => {
   const { numOfPages, page } = useSelector((store) => store.alljobs);
@@ -9,8 +10,20 @@ export const PageBtnContainer = () => {
   const pages = Array.from({ length: numOfPages }, (_, index) => {
     return index + 1;
   });
-  const nextPage = () => {};
-  const prevPage = () => {};
+  const nextPage = () => {
+    let newPage = page + 1;
+    if (newPage > numOfPages) {
+      newPage = 1;
+    }
+    dispatch(changePage(newPage));
+  };
+  const prevPage = () => {
+    let newPage = page - 1;
+    if (newPage < 1) {
+      newPage = numOfPages;
+    }
+    dispatch(changePage(newPage));
+  };
 
   return (
     <Wrapper>
@@ -25,7 +38,7 @@ export const PageBtnContainer = () => {
               type="button"
               className={pageNumber === page ? 'pageBtn active' : 'pageBtn'}
               key={pageNumber}
-              onClick={() => console.log('change page')}
+              onClick={() => dispatch(changePage(pageNumber))}
             >
               {pageNumber}
             </button>
