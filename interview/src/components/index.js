3e2ae0b62077d2ac5eca36ba0c115c8431309d49
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-07-14 11:39:55 +0300

    create search form

--- interview/src/components/index.js
+++ interview/src/components/index.js
@@ -5,7 +5,7 @@ import { SmallSidebar } from './SmallSidebar';
 import { BigSidebar } from './BigSidebar';
 import { FormRowSelect } from './FormRowSelect';
 import { JobsContainer } from './JobsContainer';
-import { SearchContainer } from './SearchContainer';
+import SearchContainer from './SearchContainer';
 import { StatsContainer } from './StatsContainer';
 import { ChartsContainer } from './ChartsContainer';
 
--- interview/src/pages/dashboard/Stats.js
+++ interview/src/pages/dashboard/Stats.js
@@ -1,26 +1,6 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { StatsContainer, Loading, ChartsContainer } from '../../components';
-import { showStats } from '../../features/alljobs/allJobsSlice';
+import React from 'react';
 
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
-    (store) => store.allJobs
-  );
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(showStats());
-  }, []);
-  if (isLoading) {
-    return <Loading center />;
-  }
-  return (
-    <>
-      <StatsContainer />
-      {monthlyApplications.length > 0 && <ChartsContainer />}
-    </>
-  );
+  return <div>Stats</div>;
 };
-
 export default Stats;
